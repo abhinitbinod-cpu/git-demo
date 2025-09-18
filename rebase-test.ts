0ab7774837b3d55f9fdd9f76251c9bd227523e04
Abhinit Binod <abhinit.10683004@ltimindtree.com>
2025-09-18 17:27:21 +0530

    commit-7

--- rebase-test.ts
+++ rebase-test.ts
@@ -3,9 +3,9 @@ class RebaseTest{
     y=20;
     z=30;
     a=10;
-
+    b=20;
     printValues() {
-        console.log(`x = ${this.x}, y = ${this.}, z = ${this.z}, a=${this.a}`);
+        console.log(`x = ${this.x}, y = ${this.}, z = ${this.z}, a=${this.a}, b=${this.b}`);
     }
 }
 
