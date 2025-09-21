06b5ca64eb39c41b474616546d9e0aa3ba3d3d4d
Abhinit Binod <abhinit.10683004@ltimindtree.com>
2025-09-21 13:15:30 +0530

    resolved conflicts

--- rebase-test.ts
+++ rebase-test.ts
@@ -3,10 +3,9 @@ class RebaseTest{
     y=20;
     z=30;
     a=10;
-    b=20;
-    c=10;
+
     printValues() {
-        console.log(`x = ${this.x}, y = ${this.}, z = ${this.z}, a=${this.a}, b=${this.b},c=${this.c}`);
+        console.log(`x = ${this.x}, y = ${this.}, z = ${this.z}, a=${this.a}`);
     }
 }
 
